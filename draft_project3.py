6bc32f63c8a2bf276eea070b1ab00dbc5d980060
pmb-7684 <pbailey21@outlook.com>
2024-10-14 13:18:05 +0000

    inital import for project

---
+++ draft_project3.py
@@ -0,0 +1,8 @@
+
+#load libraries
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+import seaborn as sns
